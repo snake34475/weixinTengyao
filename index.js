84ea2701bfe0379e6f515c4e12cd8d5b4292407a
wangtengyao <1098834475@qq.com>
2022-08-05 16:42:11 +0800

    feat:显示之前模板代码

--- index.js
+++ index.js
@@ -1,83 +1,78 @@
-// const path = require("path");
-// const express = require("express");
-// const cors = require("cors");
-// const morgan = require("morgan");
-// // const { init: initDB, Counter } = require("./db");
-// const logger = morgan("tiny");
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+// const { init: initDB, Counter } = require("./db");
+const logger = morgan("tiny");
 
-// const app = express();
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-// app.use(cors());
-// app.use(logger);
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(logger);
 
-// // 首页
-// app.get("/", async (req, res) => {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
+// 首页
+app.get("/", async (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
-// // 更新计数
-// // app.post("/api/count", async (req, res) => {
-// //   const { action } = req.body;
-// //   if (action === "inc") {
-// //     await Counter.create();
-// //   } else if (action === "clear") {
-// //     await Counter.destroy({
-// //       truncate: true,
-// //     });
-// //   }
-// //   res.send({
-// //     code: 0,
-// //     data: await Counter.count(),
-// //   });
-// // });
+// 更新计数
+app.post("/api/count", async (req, res) => {
+    const { action } = req.body;
+    if (action === "inc") {
+        await Counter.create();
+    } else if (action === "clear") {
+        await Counter.destroy({
+            truncate: true,
+        });
+    }
+    res.send({
+        code: 0,
+        data: await Counter.count(),
+    });
+});
 
-// // 获取计数
-// // app.get("/api/count", async (req, res) => {
-// //   const result = await Counter.count();
-// //   res.send({
-// //     code: 0,
-// //     data: result,
-// //   });
-// // });
-
-
-// // 小程序调用，获取微信 Open ID
-// // app.get("/api/wx_openid", async (req, res) => {
-// //   if (req.headers["x-wx-source"]) {
-// //     res.send(req.headers["x-wx-openid"]);
-// //   }
-// // });
-
-// const port = process.env.PORT || 80;
-
-// async function bootstrap() {
-//   await initDB();
-//   app.listen(port, () => {
-//     console.log("启动成功", port);
-//   });
-// }
+// 获取计数
+app.get("/api/count", async (req, res) => {
+    const result = await Counter.count();
+    res.send({
+        code: 0,
+        data: result,
+    });
+});
 
 
-// app.get("/api/concat", async (req, res) => {
-//   const result = await Counter.count();
-//   res.send({
-//     code: 0,
-//     data: result,
-//   });
-// });
-// bootstrap();
+// 小程序调用，获取微信 Open ID
+app.get("/api/wx_openid", async (req, res) => {
+    if (req.headers["x-wx-source"]) {
+        res.send(req.headers["x-wx-openid"]);
+    }
+});
+
+const port = process.env.PORT || 80;
+
+async function bootstrap() {
+    await initDB();
+    app.listen(port, () => {
+        console.log("启动成功", port);
+    });
+}
 
 
-const express = require('express')
+app.get("/api/concat", async (req, res) => {
+    const result = await Counter.count();
+    res.send({
+        code: 0,
+        data: result,
+    });
+});
+bootstrap();
+
 const bodyParser = require('body-parser')
 const axios = require('axios')
 
 const PORT = process.env.PORT || 80
 const HOST = '0.0.0.0'
-
-// App
-const app = express()
 
 app.use(bodyParser.raw())
 app.use(bodyParser.json({}))
